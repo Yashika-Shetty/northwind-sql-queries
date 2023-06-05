fb17b4a03da728e0d960e74488ded505eecef069
Yashika-Shetty <yashika.shetty_19@sakec.ac.in>
2023-06-05 10:52:39 +0530

    Categories table created

--- northwind-sql-queries/northwind.sql
+++ northwind-sql-queries/northwind.sql
@@ -1,2 +1,8 @@
 /* This is an SQL File
 */
+CREATE TABLE categories (
+    category_id int NOT NULL,
+    category_name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    PRIMARY KEY (category_id)
+); /* creating categories table */
