507c5c74382c16e5f41741259b5bef7e9728bfbe
Yashika-Shetty <yashika.shetty_19@sakec.ac.in>
2023-06-07 08:27:41 +0530

    Query returns orders never placed

---
+++ northwind-sql-queries/Query_8.sql
@@ -0,0 +1,10 @@
+/* Products that have never been ordered */
+SELECT products.product_id, products.product_name
+FROM products
+LEFT JOIN order_details ON products.product_id = order_details.product_id
+WHERE order_details.quantity ISNULL;
+
+/* 2nd way to write the query*/
+SELECT product_id, product_name
+FROM products
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM order_details);
